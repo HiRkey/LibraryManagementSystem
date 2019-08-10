3b62f29c59c982e8fc3b432434b09df487cd680c
shawn <blueshieldcross@gmail.com>
2019-08-10 01:39:17 -0400

    edit

--- LibraryManagementSystem_A2/Global.asax.cs
+++ LibraryManagementSystem_A2/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
+using System.Web.Http;
 using System.Web.Routing;
 
 namespace LibraryManagementSystem_A2
@@ -15,7 +16,6 @@ namespace LibraryManagementSystem_A2
         protected void Application_Start()
         {
             Database.SetInitializer(new LibraryDBInitializer());
-
 
             AreaRegistration.RegisterAllAreas();
             //UnityConfig.RegisterComponents();
